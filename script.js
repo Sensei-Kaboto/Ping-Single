849638bf3da9242871bd1369fbdf8bd2cc677a27
Sensei-Kaboto <spacexdataxfuture@gmail.com>
2023-12-01 18:52:56 +0200

    add js validation

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let input = document.getElementById('first');
+let errorMessage = document.getElementById('errorMessage');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', () =>{
+    let email = input.value;
+    if(!email.includes("@gmail.com")){
+        input.style.border = '1px solid  brown';
+        errorMessage.style.display = 'block';
+    }else{
+        input.style.border = '1px solid  green';
+    }
+})
+
